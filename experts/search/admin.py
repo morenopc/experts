4f9697026d4f4da17725a4ad4fd031c63394a310
Moreno Cunha <moreno.pinheiro@gmail.com>
2018-03-28 21:04:07 -0300

    Enable search model at admin

--- experts/search/admin.py
+++ experts/search/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from search.models import Expert
 
-# Register your models here.
+
+class ExpertAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Expert, ExpertAdmin)
