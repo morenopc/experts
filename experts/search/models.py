b3142444079b8d37c31f0d6e3f6c7b803e893809
Moreno Cunha <moreno.pinheiro@gmail.com>
2018-03-28 22:08:52 -0300

    Let's have a fix number of stars

--- experts/search/models.py
+++ experts/search/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 
 POPULARITY_CHOICES = (
-    (0, 0),
-    (1, 1), (2, 2), (3, 3), (4, 4), (5, 5),
-    (6, 6), (7, 7), (8, 8), (9, 9), (10, 10),
+    (0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)
 )
 
 
@@ -20,7 +18,7 @@ class Expert(models.Model):
     education = models.CharField(max_length=64, default='Harvard University')
 
     description_title = models.CharField(
-        max_length=64, default='Your future English tutor')
+        max_length=64, default='Your future English tutor!')
     description = models.TextField(default='Type your description here')
 
     hourly_rate = models.PositiveSmallIntegerField(default=35)
